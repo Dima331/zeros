e084a6a32bcbae0d80b98074020e0f84eb675046
Barkov Dzmitriy <dimbar331@gmail.com>
2019-10-06 15:09:14 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function zeros(expression) {
-  // your solution
+    expression = expression.split('*');
+
+    var tmpFive = 0;
+    var tmpTwo = 0;
+    var tmpNumber = 0;
+
+    for (var i = 0; i < expression.length; i++) {
+        if (expression[i].indexOf('!!') == -1) {
+            tmpNumber = +expression[i].replace('!', "")
+            for (var j = tmpNumber; j > 0; j--) {
+                var tmpNumber = j;
+                while (tmpNumber % 5 == 0) {
+                    tmpNumber /= 5;
+                    tmpFive++;
+                }
+                while (tmpNumber % 2 == 0) {
+                    tmpNumber /= 2;
+                    tmpTwo++;
+                }
+            }
+        } else {
+            tmpNumber = +expression[i].replace('!!', "")
+            for (var j = tmpNumber; j > 0; j -= 2) {
+                var tmpNumber = j;
+                while (tmpNumber % 5 == 0) {
+                    tmpNumber /= 5;
+                    tmpFive++;
+                }
+                while (tmpNumber % 2 == 0) {
+                    tmpNumber /= 2;
+                    tmpTwo++;
+                }
+            }
+        }
+    }
+    if (tmpTwo > tmpFive) {
+        return tmpFive
+    } else {
+        return tmpTwo
+    }
 }
